5a117a1279a8d3301bd9282ae684895acf34b3d5
msmalley <mark@smalley.my>
2019-04-14 08:49:14 +0800

    updated teaser

--- js/bloqverse.js
+++ js/bloqverse.js
@@ -16,13 +16,16 @@ var bloqverse_web = {
     },
     realign: function()
     {
+        $('#credits').height('auto');
         var h = $('#meta').height();
         var w = window.innerHeight / 2;
         var d = (h - w) + 145;
         var g = $('#grid').height();
+        var c = $('#credits').height();
         $('#container').height(h + 150);
         $('#head').height(g + d);
         $('#grid').css({'top': d + 'px'});
+        $('#credits').css({'height': ((c * 2) + 160) + 'px'});
     }
 };
 
